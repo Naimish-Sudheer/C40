5006e9e35cca9f30c913b7015fee2830a74b8a03
Naimish <naimishsudheer@gmail.com>
2020-11-14 17:31:03 +0530

    Structuring Code

---
+++ JS/form.js
@@ -0,0 +1,25 @@
+class Form{
+   constructor(){} 
+   display(){
+       var title=createElement("h1");
+        title.html("Car Racing Game")
+        title.position(130,0);
+        var input=createInput("name")
+        var button=createButton("play");
+        input.position(130,160)
+        button.position(250,200)
+
+        button.mousePressed(function(){
+            input.hide()
+            button.hide()
+            var name = input.value();
+            playerCount=playerCount+1
+            player.update(name)
+            player.updateCount( playerCount)
+            var greeting=createElement("h2");
+            greeting.html("Welcome! "+name);
+            greeting.position (130,200)
+        })
+   }
+
+}
